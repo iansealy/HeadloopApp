48b412c36fbd12fa0bd6a71f88e77928d3a324bc
Busch Lab <git@buschlab.org>
2024-07-31 04:34:44 -0700

    Typo.

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'saddling-partridge-vending'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'OoR7Seengahliu9Ohthoov2uigh7Gu2chieGeichehae5ChuLae6shei1fiepaShie4SahngahF1ci6ui8yeg3aed2quer5ahpeth3OhtheeL8ahcheiR8eghaiLiexa'
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
